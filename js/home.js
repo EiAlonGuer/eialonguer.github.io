8a8b2511078fa500404998c22bfc2dd92a7eb36a
1 <eialonguer@gmail.com>
2026-02-12 13:20:38 +0100

    A ver qué tal 4

--- js/home.js
+++ js/home.js
@@ -1,30 +1,45 @@
 const gallery = document.getElementById("gallery");
 
-window.onmousedown = e => {
-    if(gallery.dataset.mousedownAt === "0") return;
+const handleOnDown = e => gallery.dataset.mouseDownAt = e.clientX;
 
-    const mouseDelta = parseFloat(TrackEvent.dataset.mousedownAt) - e.clientX;
-    const maxDelta = window.innerWidth / 2;
-
-    const percentage = (mouseDelta / maxDelta) * -100;
-    const nextPercentage = parseFloat(gallery.dataset.prevPercentage) + percentage;
-    
-    gallery.dataset.percentage = nextPercentage;
-
-    gallery.animate(
-        {transform: `translate(${nextPercentage}, -50%)`}, 
-        {duration:1200, fill: "forwards"}
-    );
-
-    for(const image of gallery.getElementsByClassName("image")){
-        image.animate(
-            {objectPosition: `${nextPercentage + 100} 50%`},
-            {duration:1200, fill: "forwards"}
-        );
-    }
+const handleOnUp = () => {
+  gallery.dataset.mouseDownAt = "0";  
+  gallery.dataset.prevPercentage = gallery.dataset.percentage;
 }
 
-window.onmouseup = () => {
-    gallery.dataset.mousedownAt = "0";
-    gallery.dataset.prevPercentage = gallery.dataset.percentage;
+const handleOnMove = e => {
+  if(gallery.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(gallery.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nexP1 = parseFloat(gallery.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nexP1, 0), -100);
+  
+  gallery.dataset.percentage = nextPercentage;
+  
+  gallery.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of gallery.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
 }
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
